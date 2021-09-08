694ae9f76902e266b11ac6bbf1a1af453aa30287
Cristian <csanahuja10@gmail.com>
2021-09-08 15:36:44 +0200

    Add field

--- src/product/recaptcha_invisible/interfaces.py
+++ src/product/recaptcha_invisible/interfaces.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.schema.interfaces import ITextLine
 
 
 class IProductRecaptchaInvisibleLayer(IDefaultBrowserLayer):
-    """Marker interface that defines a browser layer."""
+    """ Marker interface that defines a browser layer.
+    """
+
+
+class IRecaptchaInvisibleField(ITextLine):
+    """ Marker interface for Recaptcha Invisible Field
+    """
