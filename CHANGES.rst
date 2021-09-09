aed8b7a3c660560481b50cff26638a66acdf0287
Cristian <csanahuja10@gmail.com>
2021-09-09 10:47:36 +0200

    Make release

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,15 @@
 Changelog
 =========
 
+1.1 (09/09/2021)
+------------------
+
+Fixes:
+
+- Fix widget input for easyform fields view.
+  [csanahuja]
+
+
 1.0 (09/09/2021)
 ------------------
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='product.recaptcha_invisible',
-    version='1.0',
+    version='1.1',
     description="Recaptcha Invisible suport for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
